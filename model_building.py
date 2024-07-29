feaa3985b1e64e0b8d51c8c3606995110a6601a6
Shaik abdul khadar <146209562+Shaik31@users.noreply.github.com>
2024-07-29 15:54:36 +0530

    Update model_building.py

--- model_building.py
+++ model_building.py
@@ -44,7 +44,7 @@ def create_model(df):
     model.add(Dense(1))
     optimizer = tf.keras.optimizers.Adam()
     model.compile(loss='mean_squared_error',optimizer=optimizer)
-    model.fit(X_train,y_train,validation_data=(X_test,y_test),epochs=50,batch_size=32,verbose=1)
+    model.fit(X_train,y_train,validation_data=(X_test,y_test),epochs=10,batch_size=32,verbose=1)
 
     ### Lets Do the prediction and check performance metrics
     train_predict=model.predict(X_train)
