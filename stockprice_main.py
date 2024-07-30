fbe41764d9e0c911e274a6f32343dd46412877aa
Shaik abdul khadar <146209562+Shaik31@users.noreply.github.com>
2024-07-30 22:53:11 +0530

    Update stockprice_main.py

--- stockprice_main.py
+++ stockprice_main.py
@@ -47,7 +47,8 @@ if bt:
 # Importing dataset------------------------------------------------------
     #df = yf.download('RELIANCE.NS', start=START, end=END)
     df = pd.read_csv('Relaince_stock.csv')
-    plotdf, future_predicted_values =m.create_model(df)
+    data = df.filter(['Close']).values
+    plotdf, future_predicted_values =m.create_model(data)
     df.reset_index(inplace = True)
     st.title('Reliance Stock Market Prediction')
     st.write(df)
