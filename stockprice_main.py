1bcce006562791dcf70e70ec7ffb7632500d5bab
Shaik abdul khadar <146209562+Shaik31@users.noreply.github.com>
2024-08-01 22:07:24 +0530

    Update stockprice_main.py

--- stockprice_main.py
+++ stockprice_main.py
@@ -25,7 +25,7 @@ if bt:
     reliance['Date']=pd.to_datetime(reliance['Date'],format='%Y-%m-%d')
     reliance=reliance.set_index('Date')
     df = reliance.copy()
-    plotdf,next_predicted_days_value30,next_predicted_days_value60,next_predicted_days_value90,plotdf30,plotdf60,plotdf90 =m.create_model(df)
+    plotdf=m.create_model(df)
     df.reset_index(inplace = True)
     st.title('Reliance Stock Market Prediction')
     st.write(df)
@@ -34,63 +34,62 @@ if bt:
     fig= plt.figure(figsize=(20,10))
     sns.lineplot(data=plotdf)
     st.pyplot(fig)
-    
-    st.write('select the days to predict')
-    bt30 = st.button('30 Days')
-    bt60 = st.button('60 Days')
-    bt90 = st.button('90 Days')
-
-    if bt30:
-         st.write('Forecast')
-         df30 = pd.DataFrame(next_predicted_days_value30)
-         st.markdown("### Next 30 days forecast")
-         df30.rename(columns={0: "Predicted Prices"}, inplace=True)
-         st.write(df30)
-
-         st.markdown("Forecasted Price for 30 Days")
-         fig= plt.figure(figsize=(20,10))
-         sns.lineplot(data=plotdf30)
-         st.pyplot(fig)
-    
-   
-    else:
-         st.write('Please select days to predict')
-
-    if bt60:
-         st.write('Forecast')
-         df60 = pd.DataFrame(next_predicted_days_value60)
-         st.markdown("### Next 30 days forecast")
-         df60.rename(columns={0: "Predicted Prices"}, inplace=True)
-         st.write(df30)
-
-         st.markdown("Forecasted Price for 60 Days")
-         fig= plt.figure(figsize=(20,10))
-         sns.lineplot(data=plotdf60)
-         st.pyplot(fig)
-    
-   
-    else:
-         st.write('Please select days to predict')
-
-
-    if bt30:
-         st.write('Forecast')
-         df90 = pd.DataFrame(next_predicted_days_value90)
-         st.markdown("### Next 30 days forecast")
-         df90.rename(columns={0: "Predicted Prices"}, inplace=True)
-         st.write(df30)
-
-         st.markdown("Forecasted Price for 90 Days")
-         fig= plt.figure(figsize=(20,10))
-         sns.lineplot(data=plotdf90)
-         st.pyplot(fig)
-    
-   
-    else:
-         st.write('Please select days to predict')
-
-
 
 else:
     #displayed when the button is unclicked
- st.write('Please click on the submit button to get the EDA ans Prediction') 
+     st.write('Please click on the submit button to get the EDA ans Prediction')
+
+st.write('select the days to predict')
+bt30 = st.button('30 Days')
+bt60 = st.button('60 Days')
+bt90 = st.button('90 Days')
+
+if bt30:
+    next_predicted_days_value30,plotdf30=m.create_model(df)
+    st.write('Forecast')
+    df30 = pd.DataFrame(next_predicted_days_value30)
+    st.markdown("### Next 30 days forecast")
+    df30.rename(columns={0: "Predicted Prices"}, inplace=True)
+    st.write(df30)
+
+    st.markdown("Forecasted Price for 30 Days")
+    fig= plt.figure(figsize=(20,10))
+    sns.lineplot(data=plotdf30)
+    st.pyplot(fig)
+    
+else:
+    st.write('Please select days to predict')
+
+if bt60:
+    next_predicted_days_value60,plotdf60=m.create_model(df)
+    st.write('Forecast')
+    df60 = pd.DataFrame(next_predicted_days_value60)
+    st.markdown("### Next 30 days forecast")
+    df60.rename(columns={0: "Predicted Prices"}, inplace=True)
+    st.write(df30)
+
+    st.markdown("Forecasted Price for 60 Days")
+    fig= plt.figure(figsize=(20,10))
+    sns.lineplot(data=plotdf60)
+    st.pyplot(fig)
+    
+   
+else:
+    st.write('Please select days to predict')
+
+
+if bt30:
+    next_predicted_days_value90,plotdf90=m.create_model(df)
+    st.write('Forecast')
+    df90 = pd.DataFrame(next_predicted_days_value90)
+    st.markdown("### Next 30 days forecast")
+    df90.rename(columns={0: "Predicted Prices"}, inplace=True)
+    st.write(df30)
+
+    st.markdown("Forecasted Price for 90 Days")
+    fig= plt.figure(figsize=(20,10))
+    sns.lineplot(data=plotdf90)
+    st.pyplot(fig)
+    
+else:
+    st.write('Please select days to predict')
