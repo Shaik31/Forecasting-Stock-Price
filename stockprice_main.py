282066ba955b0d715430f9f115de180d7d2555d7
Shaik abdul khadar <146209562+Shaik31@users.noreply.github.com>
2024-08-01 22:33:54 +0530

    Update stockprice_main.py

--- stockprice_main.py
+++ stockprice_main.py
@@ -4,8 +4,18 @@ import seaborn as sns
 import streamlit as st
 import model_building as m
 
+
 # Set up the Streamlit app
 st.markdown("# Reliance Stock Market Prediction")
+
+# Initialize session state if not already done
+if 'forecast_days' not in st.session_state:
+    st.session_state.forecast_days = None
+
+# Function to handle button clicks and update session state
+def handle_button_click(days):
+    st.session_state.forecast_days = days
+
 user_input = st.multiselect('Please select the stock', ['RELIANCE'])
 bt = st.button('Submit') 
 
@@ -31,9 +41,27 @@ if bt:
 
     # Forecast buttons
     st.write('Select the number of days to predict:')
-    bt30 = st.button('30 Days')
-    bt60 = st.button('60 Days')
-    bt90 = st.button('90 Days')
+    if st.button('30 Days'):
+        handle_button_click(30)
+    if st.button('60 Days'):
+        handle_button_click(60)
+    if st.button('90 Days'):
+        handle_button_click(90)
+
+
+    if st.session_state.forecast_days:
+        days = st.session_state.forecast_days
+        next_predicted_days_value,plotdf = m.create_model(df,days=days)
+        st.write(f'Forecast for {days} Days')
+        df_forecast = pd.DataFrame(next_predicted_days_value,columns=['Predicted Prices'])
+        st.write(df_forecast)
+
+        st.markdown(f"Forecasted Price for {days} Days")
+        fig = plt.figure(figsize=(20, 10))
+        sns.lineplot(data=plotdf)
+        st.pyplot(fig)
+
+    '''
 
     # Handle button clicks for forecast prediction
     if bt30:
@@ -67,7 +95,7 @@ if bt:
         st.markdown("Forecasted Price for 90 Days")
         fig = plt.figure(figsize=(20, 10))
         sns.lineplot(data=plotdf90)
-        st.pyplot(fig)
+        st.pyplot(fig)'''
 
 else:
     st.write('Please click the submit button to get the EDA and Prediction.')
